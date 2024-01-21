302f8e83886078321f60f847486ca490639f826e
Nikhil <jhanikhil881@gmail.com>
2024-01-21 11:51:13 +0530

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import Nav from './components/Navbar'
 import Home from './components/Home'
 import { BrowserRouter,Routes , Route } from "react-router-dom";
 import Dashboard from './components/Dashboard';
-<<<<<<< HEAD
 import Chatgrp from './components/Chatgrp';
-=======
 import Chatbot from './components/Chatbot';
->>>>>>> 8eb732fb11da6e3bd46617f44857ec751e92f98d
+
 
 
 
@@ -17,12 +15,11 @@ function App() {
       <Routes>
         <Route path="*" element={<Home />}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
-<<<<<<< HEAD
         <Route path="/chat" element={<Chatgrp/>}/>
 
-=======
+
         <Route path="/chatbot" element={<Chatbot/>}/>
->>>>>>> 8eb732fb11da6e3bd46617f44857ec751e92f98d
+
       </Routes>
     </BrowserRouter>
   )
