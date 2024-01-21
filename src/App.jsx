b930a88b6451523302b0080d58285f1830f2515b
nikhil <nikhilgupta7104@gmail.com>
2024-01-21 16:36:15 +0530

    user auth completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from './components/Navbar'
 import Home from './components/Home'
 import { BrowserRouter,Routes , Route } from "react-router-dom";
 import Dashboard from './components/Dashboard';
 import Chatgrp from './components/Chatgrp';
 import Chatbot from './components/Chatbot';
+import Login from './components/login';
+import Signup from './components/Signup';
 
 
 function App() {
+
+  const [isusersign,setusersign] = useState()
+  useEffect(()=>{
+    setusersign(!!localStorage.getItem('token'))
+  },[isusersign])
+
   return (
-    <BrowserRouter>
+    <div>
+      <Nav/>
       <Routes>
         <Route path="*" element={<Home />}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/chat" element={<Chatgrp/>}/>
+        <Route path="/dashboard" element={isusersign?<Dashboard/>:<Login/>}/>
+        <Route path="/chat" element={isusersign?<Chatgrp/>:<Login/>}/>
         <Route path="/chatbot" element={<Chatbot/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+
       </Routes>
-    </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from "axios";
+import {  useLocation,useNavigate } from "react-router-dom";
+
+function Login() {
+
+  const [name,setname] = useState('')
+  const [pass,setpass] = useState('')
+  const navigate = useNavigate()
+  const location = useLocation();
+  const currentPath = location.pathname;
+  
+  useEffect(()=>{
+    fetchUsers();
+    console.log(currentPath);
+
+  },[])
+
+  const fetchUsers = () => {
+    axios.get('http://localhost:3001/register').then((res)=>{
+      console.log(res);
+    }).catch((e)=>{
+      console.log('error',e);
+    })
+  }
+
+  const handleLogin =  async (event) => {
+    event.preventDefault();
+    try {
+        const response = await axios
+        .post('http://localhost:3001/login', { username:name, password:pass })
+        const token = response.data.token
+        setname('')
+        setpass('')
+        fetchUsers();
+        navigate(`/${currentPath=='/login'?'dashboard':currentPath}`)
+        window.location.reload();
+        localStorage.setItem('token', token)
+    } catch (error) {
+        console.log('Login Error', error)
+    }
+}
+  
+
+
+  return (
+    <div className=" main flex items-center justify-center bg-[#222020] w-[100vw] h-[100vh] "  >
+      <form onSubmit={handleLogin}  className="border-4  border-[#ae09dc9f] p-20 bg-[#4c20b1] max-w-[90vw] bg-opacity-[0.7] rounded-xl">
+        <div className="text-[30px] font-bold text-white ">Login </div>
+
+        <div className=" m-2 ">
+          <div className=" my-2 text-white font-semibold ">Username</div>
+          <input className=" m-2 rounded w-full " onChange={(e)=>setname(e.target.value)} name="username" /><br />
+         <div className=" my-2 text-white font-semibold ">Password</div>
+          <input type='password' className=" m-2 rounded w-full " onChange={(e)=>setpass(e.target.value)} name="password" /><br />
+
+          <button className="border-2 hover:bg-black bg-[#ae09dc9f] hover:border-white text-white rounded-xl font-bold px-4 py-2 my-4" type="submit">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login
