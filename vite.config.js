6397b3e12630282ad93bbdf70f97ceff7b64f2d4
Nikhil <jhanikhil881@gmail.com>
2024-01-25 23:29:04 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,26 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.js'),
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  
   optimizeDeps: {
     include: ['firebase/app'],
   },
