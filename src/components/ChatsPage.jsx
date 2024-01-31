2ce8967c0604d6ecf3a07ba4ab9ef0c04b1e032b
nikhil <nikhilgupta7104@gmail.com>
2024-01-31 20:33:45 +0530

    topbtn

--- src/components/ChatsPage.jsx
+++ src/components/ChatsPage.jsx
@@ -1,37 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
-// import { useMultiChatLogic } from "react-chat-engine-advanced";
-import { PrettyChatWindow } from 'react-chat-engine-pretty';
+ import { useMultiChatLogic } from "react-chat-engine-advanced";
+//import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
 
 function ChatsPage(props) {
-  // const chatprops = useMultiChatLogic('0d484187-81b3-47aa-917f-2d26123683f3', props.user.username, props.user.secret)
-  // const navigate = useNavigate()
-  // const [user,setuser] = useState('')
-  // useEffect(() => {
+  const chatprops = useMultiChatLogic('0d484187-81b3-47aa-917f-2d26123683f3', props.user.username, props.user.secret)
+  const navigate = useNavigate()
+  const [user,setuser] = useState('')
+  useEffect(() => {
 
-  //     setuser(localStorage.getItem('user'))
-  //   if (user=='student') {
-  //     var ele = document.getElementsByClassName('ce-new-chat-button')
-  //     console.log(ele[0]);
-  //     ele[0]?.remove() 
-  //   }
-  // })
+      setuser(localStorage.getItem('user'))
+    if (user=='student') {
+      var ele = document.getElementsByClassName('ce-new-chat-button')
+      console.log(ele[0]);
+      ele[0]?.remove() 
+    }
+  })
 
   return (
-    // <div className='h-[100vh]' >
-    //     <SingleChatSocket {...chatprops}/>
-    //     <MultiChatWindow {...chatprops} style={{height:"100%"}} />
-    // </div>
-    <div className=' h-[100vh]' >
-      <button onClick={() => navigate('/home')} className='fixed hover:bg-black hover:text-white font-bold bg-white rounded-md p-2 bottom-4 left-4 ' > Go Back </button>
-      <PrettyChatWindow
-        projectId="0d484187-81b3-47aa-917f-2d26123683f3"
-        username={props.user.username}
-        secret={props.user.secret}
-        style={{ height: '100%' }}
-      />
+    <div className='h-[100vh]' >
+        <SingleChatSocket {...chatprops}/>
+        <MultiChatWindow {...chatprops} style={{height:"100%"}} />
     </div>
+    // <div className=' h-[100vh]' >
+    //   <button onClick={() => navigate('/home')} className='fixed hover:bg-black hover:text-white font-bold bg-white rounded-md p-2 bottom-4 left-4 ' > Go Back </button>
+    //   <PrettyChatWindow
+    //     projectId="0d484187-81b3-47aa-917f-2d26123683f3"
+    //     username={props.user.username}
+    //     secret={props.user.secret}
+    //     style={{ height: '100%' }}
+    //   />
+    // </div>
   )
 }
 
